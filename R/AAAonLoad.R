c000a312c074291e2485a59a97cfd235c4652dfb
Martin Atzmueller <martin@atzmueller.net>
2020-04-22 13:20:02 +0000

    version 1.0

--- R/AAAonLoad.R
+++ R/AAAonLoad.R
@@ -23,6 +23,17 @@
 require(rJava)
 
 .onLoad <- function(libname, pkgname) {
-	   rJava::.jpackage(pkgname, lib.loc=libname)
-	   #.jengine(TRUE)
+	# initialize for rJava
+	rJava::.jpackage(pkgname, lib.loc=libname)
+	#.jengine(TRUE)
+	
+		
+	# test Java version
+	.jinit()
+	jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+	if(substr(jv, 1L, 2L) == "1.") {
+		jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+		if(jvn < 1.8) stop("Java >= 8 is needed for this package but not available")
+	} 
+
 }
--- tests/test.R
+++ tests/test.R
@@ -19,15 +19,6 @@
 #
 #    Contact: Martin Atzmueller (martin@atzmueller.net)
 ###############################################################################
-library(rJava)
-
-.jinit()
-jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
-if(substr(jv, 1L, 1L) == "1") {
-	jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
-	if(jvn < 1.8) stop("Java 8 is needed for this package but not available")
-} 
-
 library(rsubgroup)
 
 data(credit.data)
